ceaabfbcbb1856b703bcb4959c230257b2b0e281
Steven <Stependang@gmail.com>
2021-09-20 20:14:23 -0400

    Adding Error Messages

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -2,10 +2,44 @@ package edu.temple.signupform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val saveButton = findViewById<Button>(R.id.button)
     }
+    fun saveClicked(view: View){
+        val txtNameView = findViewById<TextView>(R.id.txtName)
+        val txtEmailView = findViewById<TextView>(R.id.txtEmail)
+        val txtPasswordView = findViewById<TextView>(R.id.txtPassword)
+        val txtPasswordConfirmView = findViewById<TextView>(R.id.txtPasswordConfirm)
+        if(txtNameView.text.isBlank()){
+            //Toast.makeText(this@FormActivity,"No name detected", Toast.LENGTH_SHORT).show()
+            txtNameView.error = getString(R.string.nameError)
+        }
+        if(txtEmailView.text.isBlank()){
+            //Toast.makeText(this@FormActivity,"No email detected", Toast.LENGTH_SHORT).show()
+            txtEmailView.error=getString(R.string.emailError)
+        }
+        if(txtPasswordView.text.isBlank()){
+            //Toast.makeText(this@FormActivity,"No password detect", Toast.LENGTH_SHORT).show()
+            txtPasswordView.error=getString(R.string.passwordError)
+        }
+        if(txtPasswordConfirmView.text.isBlank()) {
+            //Toast.makeText(this@FormActivity, "No password re-enter", Toast.LENGTH_SHORT).show()
+            txtPasswordConfirmView.error=getString(R.string.passwordConfirmError)
+        }
+        if(!(txtPasswordView.text.toString().equals(txtPasswordConfirmView.text.toString()))){
+            Toast.makeText(this@FormActivity,"Password is not the same", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
 }
